9b72e051dd422892cf892d910dc8b76071055ff3
Kdvergarab <kdvergarab@gmail.com>
2022-03-29 23:09:07 +0000

    avance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light header mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+				<img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="PokéAPI"/>
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary">Favorites</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,320 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Button, Card } from "react-bootstrap";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+<div className="abuelo">
+
+    <div className="cuadrodecartas">
+      <div className="kanto">Pokedex Kanto</div>
+      <div className="d-flex">
+      <Card className="cartas" >
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+    </div>
+    </div>
+    <div className="cuadrodecartas">
+      <div className="kanto">Pokedex Kanto</div>
+      <div className="d-flex">
+      <Card className="cartas" >
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+    </div>
+    </div>
+    <div className="cuadrodecartas">
+      <div className="kanto">Pokedex Kanto</div>
+      <div className="d-flex">
+      <Card className="cartas" >
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card  className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+      <Card className="cartas">
+        <Card.Img
+          variant="top"
+          src="https://nintendo.pe/wp-content/uploads/2018/05/f51d08be05919290355ac004cdd5c2d6.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Learn more!</Button>
+        </Card.Body>
+      </Card>
+    </div>
+    </div>
+    </div>
+    
+  
 );
